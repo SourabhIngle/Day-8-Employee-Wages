2fcff6813fcec632fd73ec5bad24c7d7085a908e
SourabhIngle <100rabhingle@gmail.com>
2023-01-26 15:09:17 +0530

    UC4

--- EmployeeWages.java
+++ EmployeeWages.java
@@ -1,33 +1,28 @@
 public class EmployeeWages {
     public static void main(String[] args) {
 
-        // CONSTANT VALUES
-        int IS_FULL_TIME = 1;
-        int IS_PART_TIME = 2;
-        final int EMP_RATE_PER_HR = 20;
-
-        //variables
-
-        int EmpWorkingHrs;
-        int TotalEarnings;
+        //constants
+        int EMP_RATE_PER_HR = 20;
+        int empWorkingHrs;
+        int dailyWage;
 
         int random = (int) Math.floor(Math.random() * 10) % 3;
         System.out.println(random);
 
-        if (IS_FULL_TIME == random) {
-            EmpWorkingHrs = 8;
-            TotalEarnings = (EmpWorkingHrs * EMP_RATE_PER_HR);
-            System.out.println("Employee is Full Time with salary: " + TotalEarnings);
-        } else if (IS_PART_TIME == random) {
-            EmpWorkingHrs = 4;
-            TotalEarnings = (EmpWorkingHrs * EMP_RATE_PER_HR);
-            System.out.println("Employee is Part Time with Salary: " + TotalEarnings);
-        } else {
-            TotalEarnings = 0;
-            System.out.println("Employee is Absent, hence salary: " + TotalEarnings);
-
+        switch (random) {
+            case 1:
+                System.out.println("Employee is Present FULL TIME.");
+                empWorkingHrs = 8;
+                break;
+            case 2:
+                System.out.println("Employee is Present Part Time.");
+                empWorkingHrs = 4;
+                break;
+            default:
+                System.out.println("Employee is ABSENT.");
+                empWorkingHrs = 0;
         }
-
+        dailyWage = (EMP_RATE_PER_HR * empWorkingHrs);
+        System.out.println("Employee has worked " + empWorkingHrs + ", hence earned a salary of Rs. " + dailyWage);
     }
-
 }
