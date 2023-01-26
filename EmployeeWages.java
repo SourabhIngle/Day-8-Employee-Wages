2fa09d3c6f25197abd0afb15b71d03e370f38110
SourabhIngle <100rabhingle@gmail.com>
2023-01-26 15:06:34 +0530

    UC3

--- EmployeeWages.java
+++ EmployeeWages.java
@@ -2,22 +2,32 @@ public class EmployeeWages {
     public static void main(String[] args) {
 
         // CONSTANT VALUES
-        int IS_PRESENT = 1;
-
+        int IS_FULL_TIME = 1;
+        int IS_PART_TIME = 2;
         final int EMP_RATE_PER_HR = 20;
-        final int EMP_HRS = 8;
 
-        // Generate random number between 0 or 1
-        int random = (int) (Math.floor(Math.random() * 10) % 2);
+        //variables
+
+        int EmpWorkingHrs;
+        int TotalEarnings;
+
+        int random = (int) Math.floor(Math.random() * 10) % 3;
         System.out.println(random);
 
-        int EmpWage;
-        if (random == IS_PRESENT) {
-            EmpWage = (EMP_RATE_PER_HR * EMP_HRS);
+        if (IS_FULL_TIME == random) {
+            EmpWorkingHrs = 8;
+            TotalEarnings = (EmpWorkingHrs * EMP_RATE_PER_HR);
+            System.out.println("Employee is Full Time with salary: " + TotalEarnings);
+        } else if (IS_PART_TIME == random) {
+            EmpWorkingHrs = 4;
+            TotalEarnings = (EmpWorkingHrs * EMP_RATE_PER_HR);
+            System.out.println("Employee is Part Time with Salary: " + TotalEarnings);
         } else {
-            EmpWage = 0;
+            TotalEarnings = 0;
+            System.out.println("Employee is Absent, hence salary: " + TotalEarnings);
+
         }
-        System.out.println("Daily Wage of the Employee is: " + EmpWage);
+
     }
 
 }
