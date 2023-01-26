9bb3b4bcfcaa8f208a12f83308e73b2a1a192572
SourabhIngle <100rabhingle@gmail.com>
2023-01-26 15:15:18 +0530

    UC6

--- EmployeeWages.java
+++ EmployeeWages.java
@@ -1,19 +1,24 @@
 public class EmployeeWages {
     public static void main(String[] args) {
 
+        System.out.println("Welcome to Employee Wages program");
+
         //constants
-        final int IS_PRESENT = 1;
-        final int IS_ABSENT = 2;
+        int IS_PRESENT = 1;
+        int IS_PART_TIME = 2;
+        int IS_ABSENT = 0;
         final int EMP_RATE_PER_HR = 20;
         final int TOTAL_WORKING_DAYS = 20;
+        final int TOTAL_WORKING_HOURS = 100;
 
         //variables;
         int empHrs;
         int totalEarnings = 0;
         int dailyWage;
         int empDays = 0;
+        int workingHrs = 0;
 
-        for (int i = 0; i < TOTAL_WORKING_DAYS; i++) {
+        for (int i = 0; i < TOTAL_WORKING_DAYS && i < TOTAL_WORKING_HOURS; i++) {
             int random = (int) Math.floor(Math.random() * 10) % 3;
             empDays++;
             switch (random) {
@@ -21,22 +26,25 @@ public class EmployeeWages {
                     empHrs = 8;
                     dailyWage = empHrs * EMP_RATE_PER_HR;
                     totalEarnings = totalEarnings + dailyWage;
-                    System.out.println("Earning till Day " + empDays + " is: " + dailyWage);
+                    workingHrs = workingHrs + empHrs;
+                    System.out.println("Earning till Day " + empDays + " is: " + totalEarnings);
                     break;
                 case 2:
                     empHrs = 4;
                     dailyWage = empHrs * EMP_RATE_PER_HR;
                     totalEarnings = totalEarnings + dailyWage;
-                    System.out.println("Earning till Day " + empDays + " is: " + dailyWage);
+                    workingHrs = workingHrs + empHrs;
+                    System.out.println("Earning till Day " + empDays + " is: " + totalEarnings);
                     break;
                 default:
                     empHrs = 0;
                     dailyWage = empHrs * EMP_RATE_PER_HR;
                     totalEarnings = totalEarnings + dailyWage;
-                    System.out.println("Earning till Day " + empDays + " is: " + dailyWage);
+                    workingHrs = workingHrs + empHrs;
+                    System.out.println("Earning till Day " + empDays + " is: " + totalEarnings);
                     break;
             }
         }
-        System.out.println("Employee has earned " + totalEarnings + " in a month");
+        System.out.println("Employee has earned " + totalEarnings + " in a month and has worked for " + workingHrs + " hours in total");
     }
 }
