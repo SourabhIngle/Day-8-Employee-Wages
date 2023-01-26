5eb73f30f11e3908f2ba39d0e1cc04ee0f3b4ce5
SourabhIngle <100rabhingle@gmail.com>
2023-01-26 14:58:44 +0530

    UC1

--- EmployeeWages.java
+++ EmployeeWages.java
@@ -1,5 +1,18 @@
 public class EmployeeWages {
     public static void main(String[] args) {
-        System.out.println("Welcome to Employee Wages Using Method");
+
+        // CONSTANT VARIABLE
+        final int IS_FULL_TIME = 1;
+
+        // Generate random number between 0 or 1.
+        int random = (int) ((Math.random() * 10) % 2);
+        System.out.println(random);
+
+        //Check employee is present or absent.
+        if (random == IS_FULL_TIME) {
+            System.out.println("Employee is Present");
+        } else {
+            System.out.println("Employee is Absent");
+        }
     }
 }
